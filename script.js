8b2c23a871d27e91d6d6a3e02b614799bc778954
xjupitarbssc <157065246+xjupitarbssc@users.noreply.github.com>
2024-01-18 13:17:26 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener('DOMContentLoaded', function () {
-    setGreeting('greetingStore1', 'Salutations, welcome to Giddy Grocer!! How can I help?');
-    setGreeting('greetingStore2', 'Salutations, welcome to Barney's! How can I help?');
-});
 
-function setGreeting(elementId, greeting) {
-    const greetingElement = document.getElementById(elementId);
-    greetingElement.textContent = greeting;
-}
